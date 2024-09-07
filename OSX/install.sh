7a33ac39bb3ad02e1e666ac0aab42bc7e37a2a31
cmassieu <cmassieu@sqli.com>
2024-09-08 00:09:57 +0200

    Add install tap cask-fonts

--- OSX/install.sh
+++ OSX/install.sh
@@ -1,35 +1,121 @@
-#!/bin/bash
+---
+- hosts: localhost
+  vars:
+    ansible_python_interpreter: /opt/homebrew/bin/python3
+  collections:
+    - community.general
 
-# Demander les droits sudo dès le début
-sudo -v
+  tasks:
+    # Ajouter le tap des polices Homebrew
+    - name: Add homebrew/cask-fonts tap
+      community.general.homebrew_tap:
+        name: homebrew/cask-fonts
+        state: present
 
-# Installation de Homebrew si non installé
-if ! command -v brew &> /dev/null; then
-    echo "Homebrew non détecté, installation en cours..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # Installer les packages Homebrew nécessaires
+    - name: Install required brew packages
+      community.general.homebrew:
+        name: "{{ item }}"
+        state: present
+      loop:
+        - wezterm
+        - vim
+        - git
+        - tmux
+        - zsh
+        - zsh-autosuggestions
+        - zsh-completions
+        - zsh-syntax-highlighting
+        - eza
+        - fzf
+        - starship
+        - zoxide
+        - thefuck
 
-    # Ajouter Homebrew au PATH
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
+    # Installer les polices requises
+    - name: Install required fonts
+      community.general.homebrew:
+        name: "{{ item }}"
+        state: present
+      loop:
+        - fontconfig
+        - font-fira-code-nerd-font
+        - font-go-mono-nerd-font
+        - font-hack-nerd-font
+        - font-jetbrains-mono-nerd-font
+        - font-meslo-lg-nerd-font
+        - font-monaspace-nerd-font
+        - font-noto-sans-symbols-2
+        - font-sf-mono-nerd-font-ligaturized  # Correct version of SF Mono Nerd Font
 
-# Installation d'Ansible si non installé
-if ! command -v ansible &> /dev/null; then
-    echo "Ansible non détecté, installation via Homebrew..."
-    brew install ansible
-fi
+    # Installer Oh My Zsh si non installé
+    - name: Install Oh My Zsh if not installed
+      shell: |
+        if [ ! -d "$HOME/.oh-my-zsh" ]; then
+          sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        fi
 
-# Installation de la collection Ansible homebrew
-echo "Installation de la collection Ansible homebrew..."
-ansible-galaxy collection install community.general
+    # Supprimer l'ancien répertoire du plugin history-substring-search s'il existe
+    - name: Remove existing history-substring-search directory if it exists
+      file:
+        path: "{{ ansible_env.HOME }}/.oh-my-zsh/plugins/history-substring-search"
+        state: absent
 
-# Exécution du playbook Ansible sans sudo pour éviter l'erreur
-echo "Exécution du playbook Ansible..."
-ansible-playbook -i localhost, -c local playbook.yml
+    # Cloner les plugins Oh My Zsh nécessaires
+    - name: Clone zsh-syntax-highlighting plugin
+      git:
+        repo: https://github.com/zsh-users/zsh-syntax-highlighting.git
+        dest: "{{ ansible_env.HOME }}/.oh-my-zsh/plugins/zsh-syntax-highlighting"
 
-# Vérifier le succès
-if [ $? -eq 0 ]; then
-    echo "Configuration terminée. Redémarre ton terminal pour appliquer les changements."
-else
-    echo "Erreur : Exécution du playbook Ansible a échoué."
-fi
+    - name: Clone zsh-autosuggestions plugin
+      git:
+        repo: https://github.com/zsh-users/zsh-autosuggestions.git
+        dest: "{{ ansible_env.HOME }}/.oh-my-zsh/plugins/zsh-autosuggestions"
+
+    - name: Clone zsh-completions plugin
+      git:
+        repo: https://github.com/zsh-users/zsh-completions.git
+        dest: "{{ ansible_env.HOME }}/.oh-my-zsh/plugins/zsh-completions"
+
+    - name: Clone history-substring-search plugin
+      git:
+        repo: https://github.com/zsh-users/zsh-history-substring-search.git
+        dest: "{{ ansible_env.HOME }}/.oh-my-zsh/plugins/history-substring-search"
+
+    # Assurer que le répertoire ~/.config existe
+    - name: Ensure ~/.config directory exists
+      file:
+        path: ~/.config
+        state: directory
+        mode: '0755'
+
+    # Copier les configurations
+    - name: Copy WezTerm config
+      copy:
+        src: files/.wezterm.lua
+        dest: ~/.wezterm.lua
+        mode: 0644
+
+    - name: Copy Tmux config
+      copy:
+        src: files/.tmux.conf
+        dest: ~/.tmux.conf
+        mode: 0644
+
+    - name: Copy Starship config
+      copy:
+        src: files/starship.toml
+        dest: ~/.config/starship.toml
+        mode: 0644
+
+    - name: Copy Vim config
+      copy:
+        src: files/.vimrc
+        dest: ~/.vimrc
+        mode: 0644
+
+    - name: Copy Zsh config
+      copy:
+        src: files/.zshrc
+        dest: ~/.zshrc
+        mode: 0644
